e2c47e0e3902446521185738769e84a170fa835c
kumarchandra1998 <kumarchandra1998@gmail.com>
2025-09-25 21:34:42 +0530

    Initial commit

---
+++ src/language/basics/HelloWorld.java
@@ -0,0 +1,16 @@
+package language.basics;
+
+public class HelloWorld {
+ // This is a main method and always JVM begins execution from main method 
+	
+	// three main types of comments
+	//1. Single-line Comment -> Starts with //
+	//2. Multi-line Comment -> Starts with /* and ends with */
+	//3. Documentation Comment -> it starts with /** and ends with */
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub (Comment in java )
+
+	}
+
+}
