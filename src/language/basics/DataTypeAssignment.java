15a1fb339876f19117e29dd00c0f1e6ef0e964db
kumarchandra1998 <kumarchandra1998@gmail.com>
2025-09-30 12:09:12 +0530

    Assigment 1 - Data Types

---
+++ src/language/basics/DataTypeAssignment.java
@@ -0,0 +1,91 @@
+package language.basics;
+
+public class DataTypeAssignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 1. Temperature of a city in degrees Celsius: 25.5
+		float temperature = 25.5f;
+		System.out.println("Temperature of city in degrees Celsius : " + temperature);
+
+		// 2. Whether a customer has placed an order: true or false
+		boolean order = true;
+		System.out.println("The order status is : " + order);
+
+		// 3. Person's phone number: "123-456-7890"
+		long PhoneNumber = 9845096068l;
+		System.out.println("The customer's phone number is : " + PhoneNumber);
+
+		// 4. Amount of money in a customer's bank account: 1000.50
+		float Amount = 100;
+		System.out.println("Amount of money in a customer's bank account : " + Amount);
+
+		// 5. Person's email address: "john.doe@example.com"
+		String email = "john.doe@example.com";
+		System.out.println("Person's email address is : " + email);
+
+		// 6. Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+		float latitudelocation = 37.7749f;
+		float longitudelocation = 122.4194f;
+		System.out.println("Coordinates of a latitude and longitude location are : " + latitudelocation + " , "
+				+ longitudelocation);
+
+		// 7. Person's marital status: true or false
+		boolean maritalstatus = true;
+		System.out.println("Person's marital status : " + maritalstatus);
+
+		// 8.Person's occupation: "Software Engineer"
+		String occupation = "Software Engineer";
+		System.out.println("Person's occupation : " + occupation);
+
+		// 9. Person's favourite colour: "Blue"
+		String colour = "Blue";
+		System.out.println("Person's favourite colour :" + colour);
+
+		// 10.Current year: 2023
+		short year = 2023;
+		System.out.println("Current year : " + year);
+
+		// 11.Number of followers on a social media platform: 1,000,000
+		String followers = "1,000,000";
+		System.out.println("Number of followers on a social media platform : " + followers);
+
+		// 12.Rating of a movie: 7.5
+		float rating = 7.5f;
+		System.out.println("Movie rating : " + rating);
+
+		// 13.Person's blood type: 'A'
+		char blood = 'A';
+		System.out.println("Person's blood type : " + blood);
+
+		// 14.Title of a book: "To Kill a Mockingbird"
+		String title = "To Kill a Mockingbird";
+		System.out.println("Title of a book : " + title);
+
+		// 15.Number of employees in a company: 500
+		short employees = 500;
+		System.out.println("Number of employees in a company : " + employees);
+
+		// 16.Time of an event: 2:30 PM
+		String time = "2:30 PM";
+		System.out.println("Time of an event : " + time);
+
+		// 17.Name of a country: "United States"
+		String name = "United States";
+		System.out.println("Name of a country : " + name);
+
+		// 18.Person's eye color: "Brown"
+		String eye = "Brown";
+		System.out.println("Person's eye color : " + eye);
+
+		// 19.Person's birthplace: "New York City"
+		String birthplace = "New York City";
+		System.out.println("Person's birthplace : " + birthplace);
+
+		// 20.Distance between two cities: 200.5
+		float distance = 200.5f;
+		System.out.println("Distance between two cities : " + distance);
+
+	}
+
+}
--- src/language/basics/HelloWorld.java
+++ src/language/basics/HelloWorld.java
@@ -1,5 +1,9 @@
 package language.basics;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.logging.Logger;
+
 public class HelloWorld {
  // This is a main method and always JVM begins execution from main method 
 	
@@ -8,8 +12,33 @@ public class HelloWorld {
 	//2. Multi-line Comment -> Starts with /* and ends with */
 	//3. Documentation Comment -> it starts with /** and ends with */
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub (Comment in java )
+		//Prints text without a newline at the end.
+		System.out.print("Hello");
+		System.out.print(" World!\n");
+   
+		//Prints text with a newline at the end.
+		System.out.println("Hello World!");
+		System.out.println("Java");
+		
+		//Functions similarly to printf but more explicitly named for formatting.
+		System.out.printf("Hello , %S What is your age %d , Do you have passport ? %b.\n ", "Kumar", 27 , true);
+		
+		//Print error messages separately from standard output.
+		System.err.print("This line is an error\n");
+		
+		//Java Logging Frameworks: For advanced logging.
+		Logger logger = Logger.getLogger("Sample logs");
+		logger.info("Currenlty line 31 is executing");
+		logger.warning("There is a warning messaing is line 32");
+		
+		//Print & Export the messages into text file.
+		PrintWriter logs = new PrintWriter("C:\\AutomationTraining\\JavaTraining\\Logs.txt");
+		logs.println("currently line 38 is prinitng");
+		logs.println("currently line 39 is prinitng");
+		logs.close();
+		
 
 	}
 
